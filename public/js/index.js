cb3010551053ca6ad953f3e727ef1f2b946fd5e0
bloom-ng <davidaremu@bloomdigitmedia.com>
2023-09-22 11:57:00 +0100

    updating public folder with images

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+currentSlideID = 1;
+sliderElement = document.getElementById('slider');
+totalSlides = sliderElement.childElementCount;
+console.log(totalSlides)
+function next(){
+
+}
+function prev(){
+
+}
